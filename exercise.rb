7720a0da9b188bbd977ffed1e1c99a0f6750370e
Ashutosh Rai <raiashutech@gmail.com>
2016-04-11 01:14:15 -0400

    revision two

--- exercise.rb
+++ exercise.rb
@@ -2,3 +2,11 @@ puts "hello India"
 a=45
 b=23
 puts a/b 
+clear
+puts " My name is Ashutosh"
+puts "add this change by git add filename"
+puts " save this in working area , but stagging still have some thing"
+puts 2*3
+print 6/7
+puts "A after adding the file in git now I changed 
+here and saved in working area"
