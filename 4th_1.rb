e3f39236bb74ada78c2d18dc04c6ec6744cc0a7b
Ashutosh Rai <raiashutech@gmail.com>
2016-04-13 07:25:09 -0400

    This is the 1st assignment

---
+++ 4th_1.rb
@@ -0,0 +1,29 @@
+#########################
+message= "Welcome"
+name= "Ruby"
+puts message+' '+name
+##########################
+
+message= "Welcome"
+name= "Ruby"
+puts message+'  '+name.upcase
+
+#############################
+
+message= "Welcome"
+name= "Ruby"
+puts message+''+name.downcase
+
+#############################
+
+message= "Welcome"
+name= "Ruby"
+puts name.reverse+'  '+message
+
+###########################
+
+message= "Welcome"
+name= "Ruby"
+puts message.upcase+''+name.upcase
+
+###############################
